76bc23224ce5e49aa54b7c0ad45dc3dbe564a152
Warren Gerald Polandra <85095564+warrenpolandra@users.noreply.github.com>
2023-04-07 15:26:23 +0700

    Initial Commit

---
+++ CurrencyConverter/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace CurrencyConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lab_title = new System.Windows.Forms.Label();
+            this.lab_from = new System.Windows.Forms.Label();
+            this.lab_to = new System.Windows.Forms.Label();
+            this.lab_amount_from = new System.Windows.Forms.Label();
+            this.tb_amount_from_value = new System.Windows.Forms.TextBox();
+            this.lab_amount_to = new System.Windows.Forms.Label();
+            this.lab_amount_to_value = new System.Windows.Forms.Label();
+            this.btn_convert = new System.Windows.Forms.Button();
+            this.btn_from_value = new System.Windows.Forms.Button();
+            this.btn_to_value = new System.Windows.Forms.Button();
+            this.btn_switch = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lab_title
+            // 
+            this.lab_title.AutoSize = true;
+            this.lab_title.BackColor = System.Drawing.Color.White;
+            this.lab_title.Font = new System.Drawing.Font("Fredoka One", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lab_title.Location = new System.Drawing.Point(396, 44);
+            this.lab_title.Name = "lab_title";
+            this.lab_title.Size = new System.Drawing.Size(439, 52);
+            this.lab_title.TabIndex = 0;
+            this.lab_title.Text = "Currency Converter";
+            // 
+            // lab_from
+            // 
+            this.lab_from.AutoSize = true;
+            this.lab_from.BackColor = System.Drawing.Color.White;
+            this.lab_from.Font = new System.Drawing.Font("Fredoka One", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lab_from.Location = new System.Drawing.Point(128, 193);
+            this.lab_from.Name = "lab_from";
+            this.lab_from.Size = new System.Drawing.Size(96, 36);
+            this.lab_from.TabIndex = 1;
+            this.lab_from.Text = "From:";
+            // 
+            // lab_to
+            // 
+            this.lab_to.AutoSize = true;
+            this.lab_to.BackColor = System.Drawing.Color.White;
+            this.lab_to.Font = new System.Drawing.Font("Fredoka One", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lab_to.Location = new System.Drawing.Point(775, 193);
+            this.lab_to.Name = "lab_to";
+            this.lab_to.Size = new System.Drawing.Size(58, 36);
+            this.lab_to.TabIndex = 2;
+            this.lab_to.Text = "To:";
+            // 
+            // lab_amount_from
+            // 
+            this.lab_amount_from.AutoSize = true;
+            this.lab_amount_from.BackColor = System.Drawing.Color.White;
+            this.lab_amount_from.Font = new System.Drawing.Font("Fredoka One", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lab_amount_from.Location = new System.Drawing.Point(86, 270);
+            this.lab_amount_from.Name = "lab_amount_from";
+            this.lab_amount_from.Size = new System.Drawing.Size(138, 36);
+            this.lab_amount_from.TabIndex = 5;
+            this.lab_amount_from.Text = "Amount:";
+            // 
+            // tb_amount_from_value
+            // 
+            this.tb_amount_from_value.Font = new System.Drawing.Font("Fredoka One", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_amount_from_value.Location = new System.Drawing.Point(236, 262);
+            this.tb_amount_from_value.Name = "tb_amount_from_value";
+            this.tb_amount_from_value.Size = new System.Drawing.Size(259, 44);
+            this.tb_amount_from_value.TabIndex = 6;
+            this.tb_amount_from_value.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tb_amount_from_value_KeyDown);
+            // 
+            // lab_amount_to
+            // 
+            this.lab_amount_to.AutoSize = true;
+            this.lab_amount_to.BackColor = System.Drawing.Color.White;
+            this.lab_amount_to.Font = new System.Drawing.Font("Fredoka One", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lab_amount_to.Location = new System.Drawing.Point(695, 270);
+            this.lab_amount_to.Name = "lab_amount_to";
+            this.lab_amount_to.Size = new System.Drawing.Size(138, 36);
+            this.lab_amount_to.TabIndex = 7;
+            this.lab_amount_to.Text = "Amount:";
+            // 
+            // lab_amount_to_value
+            // 
+            this.lab_amount_to_value.AutoSize = true;
+            this.lab_amount_to_value.BackColor = System.Drawing.Color.White;
+            this.lab_amount_to_value.Font = new System.Drawing.Font("Fredoka One", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lab_amount_to_value.Location = new System.Drawing.Point(848, 270);
+            this.lab_amount_to_value.Name = "lab_amount_to_value";
+            this.lab_amount_to_value.Size = new System.Drawing.Size(76, 36);
+            this.lab_amount_to_value.TabIndex = 8;
+            this.lab_amount_to_value.Text = "N/A";
+            // 
+            // btn_convert
+            // 
+            this.btn_convert.BackColor = System.Drawing.Color.Lime;
+            this.btn_convert.Font = new System.Drawing.Font("Fredoka One", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_convert.Location = new System.Drawing.Point(495, 390);
+            this.btn_convert.Name = "btn_convert";
+            this.btn_convert.Size = new System.Drawing.Size(239, 59);
+            this.btn_convert.TabIndex = 9;
+            this.btn_convert.Text = "Convert";
+            this.btn_convert.UseVisualStyleBackColor = false;
+            this.btn_convert.Click += new System.EventHandler(this.btn_convert_Click);
+            // 
+            // btn_from_value
+            // 
+            this.btn_from_value.BackColor = System.Drawing.SystemColors.Window;
+            this.btn_from_value.Font = new System.Drawing.Font("Fredoka One", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_from_value.Location = new System.Drawing.Point(237, 189);
+            this.btn_from_value.Name = "btn_from_value";
+            this.btn_from_value.Size = new System.Drawing.Size(148, 44);
+            this.btn_from_value.TabIndex = 10;
+            this.btn_from_value.Text = "N/A ▼";
+            this.btn_from_value.UseVisualStyleBackColor = false;
+            this.btn_from_value.Click += new System.EventHandler(this.btn_from_value_Click);
+            // 
+            // btn_to_value
+            // 
+            this.btn_to_value.BackColor = System.Drawing.SystemColors.Window;
+            this.btn_to_value.Font = new System.Drawing.Font("Fredoka One", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_to_value.Location = new System.Drawing.Point(843, 189);
+            this.btn_to_value.Name = "btn_to_value";
+            this.btn_to_value.Size = new System.Drawing.Size(148, 44);
+            this.btn_to_value.TabIndex = 11;
+            this.btn_to_value.Text = "N/A ▼";
+            this.btn_to_value.UseVisualStyleBackColor = false;
+            this.btn_to_value.Click += new System.EventHandler(this.btn_to_value_Click);
+            // 
+            // btn_switch
+            // 
+            this.btn_switch.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btn_switch.Font = new System.Drawing.Font("Fredoka One", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_switch.Location = new System.Drawing.Point(575, 178);
+            this.btn_switch.Name = "btn_switch";
+            this.btn_switch.Padding = new System.Windows.Forms.Padding(5, 0, 0, 0);
+            this.btn_switch.Size = new System.Drawing.Size(77, 67);
+            this.btn_switch.TabIndex = 12;
+            this.btn_switch.Text = "↔";
+            this.btn_switch.UseVisualStyleBackColor = false;
+            this.btn_switch.Click += new System.EventHandler(this.btn_switch_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+            this.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.BackgroundImage = global::CurrencyConverter.Properties.Resources.dollar;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1232, 553);
+            this.Controls.Add(this.btn_switch);
+            this.Controls.Add(this.btn_to_value);
+            this.Controls.Add(this.btn_from_value);
+            this.Controls.Add(this.btn_convert);
+            this.Controls.Add(this.lab_amount_to_value);
+            this.Controls.Add(this.lab_amount_to);
+            this.Controls.Add(this.tb_amount_from_value);
+            this.Controls.Add(this.lab_amount_from);
+            this.Controls.Add(this.lab_to);
+            this.Controls.Add(this.lab_from);
+            this.Controls.Add(this.lab_title);
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(0, 0, 5, 0);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Currency Converter App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lab_title;
+        private System.Windows.Forms.Label lab_from;
+        private System.Windows.Forms.Label lab_to;
+        private System.Windows.Forms.Label lab_amount_from;
+        private System.Windows.Forms.TextBox tb_amount_from_value;
+        private System.Windows.Forms.Label lab_amount_to;
+        private System.Windows.Forms.Label lab_amount_to_value;
+        private System.Windows.Forms.Button btn_convert;
+        private System.Windows.Forms.Button btn_from_value;
+        private System.Windows.Forms.Button btn_to_value;
+        private System.Windows.Forms.Button btn_switch;
+    }
+}
+
---
+++ CurrencyConverter/Form1.cs
@@ -0,0 +1,381 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.Net;
+using System.Diagnostics;
+
+namespace CurrencyConverter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string APIKey = "bsSlFZvE4WokZlLKR3wEFmoH0pZBoSQssZxEQluI";
+
+        private void btn_convert_Click(object sender, EventArgs e)
+        {
+            getConversion();
+        }
+
+        private void getConversion()
+        {
+            using (WebClient web = new WebClient())
+            {
+                if (btn_from_value.Text == "N/A ▼" || btn_to_value.Text == "N/A ▼")
+                {
+                    lab_amount_to_value.Text = "Select currency!";
+                    return;
+                }
+                if (!IsDigitsOnly(tb_amount_from_value.Text))
+                {
+                    lab_amount_to_value.Text = "Wrong amount format!\nEnter digits only";
+                    return;
+                }
+                if (tb_amount_from_value.Text == "")
+                {
+                    lab_amount_to_value.Text = "Enter amount!";
+                    return;
+                }
+                string from_val = btn_from_value.Text;
+                from_val = from_val[0].ToString() + from_val[1].ToString() + from_val[2];
+                string to_val = btn_to_value.Text;
+                to_val = to_val[0].ToString() + to_val[1].ToString() + to_val[2];
+                string url = string.Format("https://api.freecurrencyapi.com/v1/latest?apikey={0}&currencies={1}&base_currency={2}", APIKey, to_val, from_val);
+
+                var json = web.DownloadString(url);
+                Currency.Root info = JsonConvert.DeserializeObject<Currency.Root>(json);
+                double result = double.Parse(tb_amount_from_value.Text);
+                double first_result = result;
+                switch (to_val)
+                {
+                    case "AUD":
+                        result = result * info.data.AUD;
+                        break;
+                    case "BGN":
+                        result = result * info.data.BGN;
+                        break;
+                    case "BRL":
+                        result = result * info.data.BRL;
+                        break;
+                    case "CAD":
+                        result = result * info.data.CAD;
+                        break;
+                    case "CHF":
+                        result = result * info.data.CHF;
+                        break;
+                    case "CNY":
+                        result = result * info.data.CNY;
+                        break;
+                    case "CZK":
+                        result = result * info.data.CZK;
+                        break;
+                    case "DKK":
+                        result = result * info.data.DKK;
+                        break;
+                    case "EUR":
+                        result = result * info.data.EUR;
+                        break;
+                    case "GBP":
+                        result = result * info.data.GBP;
+                        break;
+                    case "HKD":
+                        result = result * info.data.HKD;
+                        break;
+                    case "HRK":
+                        result = result * info.data.HRK;
+                        break;
+                    case "HUF":
+                        result = result * info.data.HUF;
+                        break;
+                    case "IDR":
+                        result = result * info.data.IDR;
+                        break;
+                    case "ILS":
+                        result = result * info.data.ILS;
+                        break;
+                    case "INR":
+                        result = result * info.data.INR;
+                        break;
+                    case "ISK":
+                        result = result * info.data.ISK;
+                        break;
+                    case "JPY":
+                        result = result * info.data.JPY;
+                        break;
+                    case "KRW":
+                        result = result * info.data.KRW;
+                        break;
+                    case "MXN":
+                        result = result * info.data.MXN;
+                        break;
+                    case "MYR":
+                        result = result * info.data.MYR;
+                        break;
+                    case "NOK":
+                        result = result * info.data.NOK;
+                        break;
+                    case "NZD":
+                        result = result * info.data.NZD;
+                        break;
+                    case "PHP":
+                        result = result * info.data.PHP;
+                        break;
+                    case "PLN":
+                        result = result * info.data.PLN;
+                        break;
+                    case "RON":
+                        result = result * info.data.RON;
+                        break;
+                    case "RUB":
+                        result = result * info.data.RUB;
+                        break;
+                    case "SEK":
+                        result = result * info.data.SEK;
+                        break;
+                    case "SGD":
+                        result = result * info.data.SGD;
+                        break;
+                    case "THB":
+                        result = result * info.data.THB;
+                        break;
+                    case "TRY":
+                        result = result * info.data.TRY;
+                        break;
+                    case "USD":
+                        result = result * info.data.USD;
+                        break;
+                    case "ZAR":
+                        result = result * info.data.ZAR;
+                        break;
+                    default:
+                        lab_amount_to_value.Text = "N/A";
+                        break;
+                }
+                lab_amount_to_value.Text = convertNumber(result.ToString("0.00"));
+            }
+        }
+
+        private bool IsDigitsOnly(string str)
+        {
+            foreach (char c in str)
+            {
+                if (c < '0' || c > '9')
+                    return false;
+            }
+
+            return true;
+        }
+
+        private string convertNumber(string number)
+        {
+            int index;
+            string start;
+            string end;
+            if (number.Contains(','))
+            {
+                index = number.IndexOf(',');
+                Debug.WriteLine(index);
+            }
+            else
+            {
+                index = number.Length;
+            }
+            while (index > 3)
+            {
+                index -= 3;
+                end = number.Substring(index, number.Length - index);
+                start = number.Substring(0, index);
+                number = start + '.' + end;
+            }
+            return number;
+        }
+
+        private void tb_amount_from_value_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.Handled = true;
+                btn_convert_Click(this, new EventArgs());
+            }
+        }
+
+        private void btn_from_value_Click(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+
+            List<string> currencies = new List<string>();
+            currencies.Add("AUD");
+            currencies.Add("BGN");
+            currencies.Add("BRL");
+            currencies.Add("CAD");
+            currencies.Add("CHF");
+            currencies.Add("CNY");
+            currencies.Add("CZK");
+            currencies.Add("DKK");
+            currencies.Add("EUR");
+            currencies.Add("GBP");
+            currencies.Add("HKD");
+            currencies.Add("HRK");
+            currencies.Add("HUF");
+            currencies.Add("IDR");
+            currencies.Add("ILS");
+            currencies.Add("INR");
+            currencies.Add("ISK");
+            currencies.Add("JPY");
+            currencies.Add("KRW");
+            currencies.Add("MXN");
+            currencies.Add("MYR");
+            currencies.Add("NOK");
+            currencies.Add("NZD");
+            currencies.Add("PHP");
+            currencies.Add("PLN");
+            currencies.Add("RON");
+            currencies.Add("RUB");
+            currencies.Add("SEK");
+            currencies.Add("SGD");
+            currencies.Add("THB");
+            currencies.Add("TRY");
+            currencies.Add("USD");
+            currencies.Add("ZAR");
+
+            var listViewItems = currencies.Select(x => new ListViewItem(x, 0)).ToArray();
+
+            ListView listView = new ListView();
+            listView.View = View.SmallIcon;
+            listView.MultiSelect = false;
+
+            listView.Items.AddRange(listViewItems);
+            int itemToShow = 10;
+            var lastItemToShow = listViewItems.Take(itemToShow).Last();
+            int height = lastItemToShow.Bounds.Bottom + listView.Margin.Top;
+            listView.Height = height;
+            listView.HotTracking = true;
+            listView.Activation = ItemActivation.OneClick;
+            listView.ItemActivate += new EventHandler(listView_ItemActivate);
+            var popup = new ToolStripDropDown();
+            popup.AutoSize = false;
+            popup.Margin = Padding.Empty;
+            popup.Padding = Padding.Empty;
+            ToolStripControlHost host = new ToolStripControlHost(listView);
+            host.Margin = Padding.Empty;
+            host.Padding = Padding.Empty;
+            host.AutoSize = false;
+            host.Size = listView.Size;
+            popup.Size = listView.Size;
+            popup.Items.Add(host);
+
+            popup.Show(this, button.Left, button.Bottom);
+        }
+
+        private void listView_ItemActivate(object sender, EventArgs e)
+        {
+            var listView = sender as ListView;
+            var item = listView.SelectedItems[0].ToString();
+            var dropdown = listView.Parent as ToolStripDropDown;
+
+            listView.SelectedIndexChanged -= listView_ItemActivate;
+
+            dropdown.Close();
+
+            btn_from_value.Text = item[15].ToString() + item[16].ToString() + item[17] + " ▼";
+        }
+
+        private void btn_to_value_Click(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+
+            List<string> currencies = new List<string>();
+            currencies.Add("AUD");
+            currencies.Add("BGN");
+            currencies.Add("BRL");
+            currencies.Add("CAD");
+            currencies.Add("CHF");
+            currencies.Add("CNY");
+            currencies.Add("CZK");
+            currencies.Add("DKK");
+            currencies.Add("EUR");
+            currencies.Add("GBP");
+            currencies.Add("HKD");
+            currencies.Add("HRK");
+            currencies.Add("HUF");
+            currencies.Add("IDR");
+            currencies.Add("ILS");
+            currencies.Add("INR");
+            currencies.Add("ISK");
+            currencies.Add("JPY");
+            currencies.Add("KRW");
+            currencies.Add("MXN");
+            currencies.Add("MYR");
+            currencies.Add("NOK");
+            currencies.Add("NZD");
+            currencies.Add("PHP");
+            currencies.Add("PLN");
+            currencies.Add("RON");
+            currencies.Add("RUB");
+            currencies.Add("SEK");
+            currencies.Add("SGD");
+            currencies.Add("THB");
+            currencies.Add("TRY");
+            currencies.Add("USD");
+            currencies.Add("ZAR");
+
+            var listViewItems = currencies.Select(x => new ListViewItem(x, 0)).ToArray();
+
+            ListView listView = new ListView();
+            listView.View = View.SmallIcon;
+            listView.MultiSelect = false;
+
+            listView.Items.AddRange(listViewItems);
+            int itemToShow = 10;
+            var lastItemToShow = listViewItems.Take(itemToShow).Last();
+            int height = lastItemToShow.Bounds.Bottom + listView.Margin.Top;
+            listView.Height = height;
+            listView.HotTracking = true;
+            listView.Activation = ItemActivation.OneClick;
+            listView.ItemActivate += new EventHandler(listView_ItemActivate_to);
+            var popup = new ToolStripDropDown();
+            popup.AutoSize = false;
+            popup.Margin = Padding.Empty;
+            popup.Padding = Padding.Empty;
+            ToolStripControlHost host = new ToolStripControlHost(listView);
+            host.Margin = Padding.Empty;
+            host.Padding = Padding.Empty;
+            host.AutoSize = false;
+            host.Size = listView.Size;
+            popup.Size = listView.Size;
+            popup.Items.Add(host);
+
+            popup.Show(this, button.Left, button.Bottom);
+        }
+        private void listView_ItemActivate_to(object sender, EventArgs e)
+        {
+            var listView = sender as ListView;
+            var item = listView.SelectedItems[0].ToString();
+            var dropdown = listView.Parent as ToolStripDropDown;
+
+            listView.SelectedIndexChanged -= listView_ItemActivate_to;
+
+            dropdown.Close();
+
+            btn_to_value.Text = item[15].ToString() + item[16].ToString() + item[17] + " ▼";
+        }
+
+        private void btn_switch_Click(object sender, EventArgs e)
+        {
+            string val = btn_from_value.Text;
+            btn_from_value.Text = btn_to_value.Text;
+            btn_to_value.Text = val;
+            tb_amount_from_value.Text = "";
+            lab_amount_to_value.Text = "N/A";
+        }
+    }
+}
